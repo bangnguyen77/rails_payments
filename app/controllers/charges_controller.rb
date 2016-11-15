19250d8d8302712ecdc61aafddac41a43538daa9
Bang Nguyen <bangnv77@gmail.com>
2016-11-14 23:50:05 -0800

    display product information from the database

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,7 @@
 class ChargesController < ApplicationController
 
 	def create
+		product = Product.find_by_sku("kittyOne")
 		# Amount in cents
 
 		customer = Stripe::Customer.create(
@@ -16,8 +17,8 @@ class ChargesController < ApplicationController
 		)
 
 		purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
-			amount: params[:amount], description: charge.description, currency: charge.currency,
-			customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
+			amount: product.price_in_cents, description: charge.description, currency: charge.currency,
+			customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 		redirect_to purchase
 
